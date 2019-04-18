969539bdfeca1fa22a17607d0678e7cbfdf985e2
ArshiaSaleem98 <45390300+ArshiaSaleem98@users.noreply.github.com>
2019-04-18 18:41:38 +0200

    Add files via upload

---
+++ graphics1/src/graphics1/PrincipalClass.java
@@ -0,0 +1,17 @@
+package graphics1;
+
+import graphics1.Designs;
+
+import java.io.IOException;
+
+public class PrincipalClass {
+
+    public static void main(String[] args) throws IOException {
+
+        Designs v2 = new Designs();
+        v2.setVisible(true);
+
+    }
+}
+
+
